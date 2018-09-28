97bf924bec4e98f017d588db71faf234807d34da
TeriForey <teri.forey@gmail.com>
2018-09-28 14:07:29 +0100

    Edited js so that multiple KML files can be dropped onto the map and their geography saved. Works with existing data too.

--- eamena/eamena/media/js/admin_group.js
+++ eamena/eamena/media/js/admin_group.js
@@ -2,10 +2,9 @@ var WGS84 = new OpenLayers.Projection("EPSG:4326");
 var Mercator = new OpenLayers.Projection("EPSG:900913");
 //enable drag and drop of kml files onto django admin map view
 django.jQuery( document ).ready(function() {
-    console.log( "ready!" );
-    console.log('loaded');
+    console.log( "ready!");
 
-    var mapEl = django.jQuery('#id_geom_map')[0];
+    var mapEl = django.jQuery('#id_geom')[0];
 
     if(mapEl) {
         mapEl.ondrop = function (evt) {
@@ -26,6 +25,17 @@ django.jQuery( document ).ready(function() {
     }
 
 
+    var allfeatures = [];
+
+    var existingfeatures = olwidget_id_geom.vectorLayers[0].features;
+    for (var i =0; i < existingfeatures.length; i++){
+        if (existingfeatures[i].geometry) {
+            existingfeatures[i].geometry.transform(Mercator, WGS84);
+            allfeatures.push(existingfeatures[i]);
+        }
+    }
+
+
     var handleFile = function(file) {
 
         var reader = new FileReader();
@@ -40,27 +50,49 @@ django.jQuery( document ).ready(function() {
             var engine;
             var formats = ['KML', 'GPX', 'OSM'];
             for (var i = 0; i < formats.length; i++) {
-                 engine = new OpenLayers.Format[formats[i]]({
-                     internalProjection: WGS84,
-                     externalProjection: WGS84,
-                     extractStyles: true
-                 });
-                 try {
-                     results = engine.read(content);
-                 } catch (e) {}
-                 if (results && results.length) {
-                     break;
-                 }
-             }
-             if (!results || !results.length) {
-                 readerror();
-                 return;
-             }
+                engine = new OpenLayers.Format[formats[i]]({
+                    internalProjection: WGS84,
+                    externalProjection: WGS84,
+                    extractStyles: true
+                });
+                try {
+                    results = engine.read(content);
+                } catch (e) {}
+                if (results && results.length) {
+                    break;
+                }
+            }
+            if (!results || !results.length) {
+                readerror();
+                return;
+            }
 
-            //geodjango_geom is a global var created by the geodjango admin view
-            geodjango_geom.clearFeatures()
-            geodjango_geom.layers.vector.addFeatures(results);
-            geodjango_geom.map.zoomToExtent(geodjango_geom.layers.vector.getDataExtent());
+            for (var i = 0; i < results.length; i++){
+                allfeatures.push(results[i]);
+            }
+
+
+            var options = {
+                'internalProjection': new OpenLayers.Projection("EPSG:4326"),
+                'externalProjection': new OpenLayers.Projection("EPSG:4326")
+            };
+            var wktFormat = new OpenLayers.Format.WKT(options);
+            var wkttext = wktFormat.write(allfeatures);
+
+            for (var i =0; i < olwidget_id_geom.layers.length; i++){
+                if (olwidget_id_geom.layers[i].CLASS_NAME == "olwidget.EditableLayer"){
+                    var mylayr = olwidget_id_geom.layers[i];
+                    mylayr.clearFeatures();
+                    if (mylayr.textarea.value.length > 0) {
+                        mylayr.textarea.value = mylayr.textarea.value + ',' + wkttext;
+                    }else{
+                        mylayr.textarea.value = wkttext;
+                    }
+                    mylayr.readWKT();
+                }
+            }
+            olwidget_id_geom.initCenter();
+
         };
         reader.readAsText(file);
     };
