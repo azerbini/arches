edaedfcafe22d63bcb069c7b0404a98337acf913
TeriForey <teri.forey@gmail.com>
2018-10-01 16:35:12 +0100

    Removed loop to pullout Resource class, instead raw ES results are exported to JSON

--- arches/app/utils/data_management/resources/formats/archesjson.py
+++ arches/app/utils/data_management/resources/formats/archesjson.py
@@ -30,26 +30,8 @@ class JsonWriter(Writer):
         iso_date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
         json_file_name = os.path.join('{0}_{1}.{2}'.format('EAMENA', iso_date, 'json'))
         f = StringIO()
-
-
-        for count, resource in enumerate(resources, 1):
-            if count % 1000 == 0:
-                print "%s Resources exported" % count            
-            errors = []
-            
-            try:
-                a_resource = Resource().get(resource['_id'])
-                
-                a_resource.form_groups = None
-                json_resources.append(a_resource)
-            except Exception as e:
-                if e not in errors:
-                    errors.append(e)
-        if len(errors) > 0:
-            print errors[0], ':', len(errors)
                    
-                   
-        f.write((JSONSerializer().serialize({'resources':json_resources}, indent = 4, separators=(',',':'))))
+        f.write((JSONSerializer().serialize({'resources': resources}, indent = 4, separators=(',',':'))))
         json_resources_for_export.append({'name': json_file_name, 'outputfile': f})
         return json_resources_for_export
         
